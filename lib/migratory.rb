6a1d8364c41358f9689a5d64a925bab7de87c91f
Ivan Shamatov <status.enable@gmail.com>
2019-06-08 22:01:31 +0300

    initial commit

---
+++ lib/migratory.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'migratory/version'
+
+module Migratory
+  # Your code goes here...
+end
---
+++ lib/migratory/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Migratory
+  VERSION = '0.1.0'
+end
